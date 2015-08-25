e41cbc60db78b8e41d883d72efcdd04348b699e2
jo <jyokako@gmail.com>
2015-08-26 00:45:39 +0900

    change index.js

--- dataVisual/testApp/routes/index.js
+++ dataVisual/testApp/routes/index.js
@@ -4,6 +4,11 @@ NCMB = require('/root/kGarten/dataVisual/ncmb-latest.min.js').NCMB;
 var APP_KEY ="e49df7b0dad5df5a3665445beca1dd71467d72ef0e33fbe3bf5f17a4439c1e75";
 var CLIENT_KEY ="ec8e98281432001b6fb56867a9c558c0c555a19da5b43c8fdd1c7f514f71db02";
 NCMB.initialize(APP_KEY,CLIENT_KEY);
+var iid = "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk";
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
 var GameScore = NCMB.Object.extend("TestClass");
 var query = new NCMB.Query(GameScore);
 var iid ;
@@ -22,9 +27,8 @@ query.find({
   }
 });
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log("iid iis "+iid);
+
+  console.log("id iis "+iid);
   res.render('index', { title: iid });
 });
 
