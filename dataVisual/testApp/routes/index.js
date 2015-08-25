5a752b739affb3188cd3736ddf55bd32d3e2e05b
jo <jyokako@gmail.com>
2015-08-26 01:34:21 +0900

    change index.js

--- dataVisual/testApp/routes/index.js
+++ dataVisual/testApp/routes/index.js
@@ -4,32 +4,22 @@ NCMB = require('/root/kGarten/dataVisual/ncmb-latest.min.js').NCMB;
 var APP_KEY ="e49df7b0dad5df5a3665445beca1dd71467d72ef0e33fbe3bf5f17a4439c1e75";
 var CLIENT_KEY ="ec8e98281432001b6fb56867a9c558c0c555a19da5b43c8fdd1c7f514f71db02";
 NCMB.initialize(APP_KEY,CLIENT_KEY);
-var iid = "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
 var GameScore = NCMB.Object.extend("TestClass");
 var query = new NCMB.Query(GameScore);
-console.log("*****enter*****");
 query.find({
   success: function(results) {
-    console.log("Successfully retrieved " + results.length + " scores.");
-   // for (var i = 0; i < results.length; i++) {
-     // var object = results[i];
-     // iid = i;
-     //  console.log(object.id + " - " + object.get("localname"));
-    
-    //}
-  console.log("id is "+iid);
-  res.render('index', { title: results });
+  console.log("Successfully retrieved " + results.length + " scores.");
+  res.render('index', { infoList: results });
   },
   error: function(error) {
     // エラー
     console.log("can not find err is "+error.message);
   }
 });
-console.log("*****out*****");
 
 });
 
