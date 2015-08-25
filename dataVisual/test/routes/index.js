1c243aedd14bc4f4d7ec64c244fdeac46a342cfe
jo <jyokako@gmail.com>
2015-08-25 20:15:58 +0900

    change path sdk

--- dataVisual/test/routes/index.js
+++ dataVisual/test/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-NCMB = require('../ncmb-latest.min.js').NCMB;
+NCMB = require('./ncmb-latest.min.js').NCMB;
 var APP_KEY ="e49df7b0dad5df5a3665445beca1dd71467d72ef0e33fbe3bf5f17a4439c1e75";
 var CLIENT_KEY ="ec8e98281432001b6fb56867a9c558c0c555a19da5b43c8fdd1c7f514f71db02";
 NCMB.initialize(APP_KEY, CLIENT_KEY);
